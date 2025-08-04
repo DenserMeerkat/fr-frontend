6c8f8a9ecdcd9150dcf4f8d2b05fb97135ac7a63
DenserMeerkat <mragulmanoharan@gmail.com>
2025-08-04 19:59:41 +0530

    [feat] stock trend cards

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,10 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { ChartAreaInteractive } from "@/components/dashboard/chart-area-interactive";
-import { DataTable } from "@/components/dashboard/data-table";
-import { SectionCards } from "@/components/dashboard/section-cards";
-
-import data from "@/constants/data.json";
+import StockTrendCards from "@/components/dashboard/stock-trend-cards";
 
 export const Route = createFileRoute("/dashboard/")({
   component: Dashboard,
@@ -15,11 +11,7 @@ export default function Dashboard() {
     <main className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <SectionCards />
-          <div className="px-4 lg:px-6">
-            <ChartAreaInteractive />
-          </div>
-          <DataTable data={data} />
+          <StockTrendCards />
         </div>
       </div>
     </main>
